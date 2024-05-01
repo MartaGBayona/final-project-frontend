cb39d69f50b52e7431db787f349f862759b54655
Marta Gimeno Bayona <marta.g.bayona@outlook.es>
2024-05-01 12:59:20 +0200

    feat: implement redux on main.jsx and create userSlice successfully

---
+++ src/services/app/slices/userSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+        credentials: {},
+
+    },
+    reducers: {
+        login: (state, action) => {
+            return {
+                ...state,
+                ...action.payload
+            };
+        },
+        logout: (state, action) => {
+            return {
+                ...state,
+                ...action.payload
+            };
+        },
+        updated: (state, action) => {
+            return {
+                ...state,
+                ...action.payload
+            };
+        }
+    },
+});
+
+export const { login, logout, updated} = userSlice.actions;
+
+export const userData = (state) => state.user;
+
+export default userSlice.reducer;
