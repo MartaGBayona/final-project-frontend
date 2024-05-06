66721dfde815b20890a5738b0e1d5ddba085c4b4
Marta Gimeno Bayona <marta.g.bayona@outlook.es>
2024-05-06 09:45:42 +0200

    update: add some spaces on code

--- src/app/slices/userSlice.js
+++ src/app/slices/userSlice.js
@@ -30,6 +30,4 @@ export const userSlice = createSlice({
 
 export const { login, logout, updated} = userSlice.actions;
 
-export const userData = (state) => state.user;
-
 export default userSlice.reducer;
